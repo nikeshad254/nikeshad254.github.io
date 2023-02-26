059ec6dd3d3bde4729560a3562e60ec359e4eaff
Nikesh Adhikari <110375186+ultimatum254@users.noreply.github.com>
2023-02-26 11:11:07 +0545

    adding about section and some floating animation and other animations

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
+// home image changing handeler
+
 let homeImg = document.querySelector("#homeImg");
 
 const homeFlipImgs = [
+    {
+        msg: "first message",
+        img: "../medias/homeImg1.png"
+    },
+
+    {
+        msg: "second msg",
+        img: "../medias/homeImg2.png"
+    }
 
 ]
 
+let i=0;
 let imgFilp = setInterval(() => {
     homeImg.classList.toggle("active");
-}, 3000);
+    homeImg.innerHTML = `
+        <p class="message">${homeFlipImgs[i]["msg"]}</p>
+        <img id="homeImg_image" src="${homeFlipImgs[i]["img"]}" alt="">
+    `
+    i++;
+    if(i == homeFlipImgs.length){
+        i = 0;
+    }
+}, 6000);
